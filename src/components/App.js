8fc80656e55564d736e22e2429bcbff64c2fbc84
gerardmi24 <gerardmistretta@yahoo.com>
2021-01-17 15:43:31 -0500

    commit tree dd8fa65245ae3d99f7ce8726d87cfbffd8d6420b

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React, {useState, useEffect} from "react"; 
 
 function App() {
+    const [defaultPic, setDefaultPic] = useState(null);
     
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((resp) => resp.json())
+        .then((data) => {setDefaultPic(data.message)
+        });
+    }, []);
+
+    if (!defaultPic) return <p>Loading...</p>
+
     return (
-        <div>
-            
-        </div>
+            <img src={defaultPic} alt="A Random Dog" />
     )
 }
 
